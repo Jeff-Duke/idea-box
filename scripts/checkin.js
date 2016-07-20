dc5c8da88e48d46fa6aed1a153fc3d1d1839a800
Jeff Duke <jeff.duke@gmail.com>
2016-07-20 15:28:19 -0600

    add quality buttons.  create quality functionality and click events.

---
+++ scripts/checkin.js
@@ -0,0 +1,20 @@
+function KidOnTheBlock(x, y) {
+  this.x = x;
+  this.y = y;
+  this.guitarVolume = 0;
+}
+
+KidOnTheBlock.prototype.riffGuitar = function() {
+  return this.guitarVolume++
+}
+
+var Donnie = new KidOnTheBlock(0,0)
+var Jordan = new KidOnTheBlock(10,10)
+
+Donnie
+  x value
+  y value
+  guitarVolume
+  riffGuitar()
+
+  
