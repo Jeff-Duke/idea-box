67862e0ad6a971ce63de00ca8a449813b254a638
swanie21 <kswanie21@gmail.com>
2016-07-21 16:57:50 -0600

    refactor object

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,6 @@
 var $saveButton = $('.save-button');
 var $searchInput = $('.search-input');
 var $ideaContainer = $('.idea-container');
-var ideasArray = [];
-
-$(document).ready(function() {
-  var storedIdeas = JSON.parse(localStorage.getItem('ideasArray'));
-  if (storedIdeas) { ideasArray = storedIdeas; }
-  ideasToPage();
-});
 
 function Idea(title, body) {
   this.title = title;
@@ -16,89 +9,105 @@ function Idea(title, body) {
   this.id = Date.now();
 }
 
-function generateNewIdea() {
-  var $titleInput = $('.title-input').val();
-  var $bodyInput = $('.body-input').val();
-  var idea = new Idea($titleInput, $bodyInput);
-
-  ideasArray.push(idea);
-
-  storeTheArray();
-  ideasToPage();
-}
-function storeTheArray() {
-  localStorage.setItem('ideasArray', JSON.stringify(ideasArray));
-}
-
-function ideasToPage() {
-  for (var i = 0; i < ideasArray.length; i++) {
-    var storedIdea = ideasArray[i];
-
-    $(`
-      <article class="idea" id=${storedIdea.id}>
-        <header class= "idea-header">
-          <h3 class="idea-title">${storedIdea.title}</h3>
-            <section class="idea-header-buttons">
-              <button class="remove-idea" type="button"></button>
-            </section>
-        </header>
-        <body class="idea-body">
-          <p class="idea-body">${storedIdea.body}</p>
-        </body>
-        <footer class="idea-footer">
-          <button class="thumbs-up" type="button"></button>
-          <button class="thumbs-down" type="button"></button>
-          <p>ranking: <span class="ranking">${storedIdea.quality}</span></p>
-        </footer>
-      </article>`).prependTo($ideaContainer);
-  }
-}
-
-function clearInput() {
-  $('.title-input').val('');
-  $('.body-input').val('');
-  $('.title-input').focus();
-}
-
-function thumbsUpRanking() {
+Idea.prototype.upVote = function() {
   var $ideaElement = $(this).parents('.idea-footer');
   var $rankingElement = $ideaElement.find('.ranking');
   var ranking = $rankingElement.text();
-
   if (ranking == 'plausible') {
     $rankingElement.text('genius');
   }
   if (ranking == 'swill') {
     $rankingElement.text('plausible');
   }
+};
 
-}
-
-function thumbsDownRanking() {
+Idea.prototype.downVote = function() {
   var $ideaElement = $(this).parents('.idea-footer');
   var $rankingElement = $ideaElement.find('.ranking');
   var ranking = $rankingElement.text();
-
   if (ranking == 'plausible') {
     $rankingElement.text('swill');
   }
   if (ranking == 'genius') {
     $rankingElement.text('plausible');
   }
-}
+};
+
+$ideaContainer.on('click', '.thumbs-up', thumbsUpRanking);
+
+$ideaContainer.on('click', '.thumbs-down', thumbsDownRanking);
 
 $saveButton.on('click', function(event) {
   event.preventDefault();
-  generateNewIdea();
+  Ideas.generateNewIdea();
+  Ideas.storeTheArray();
+  Ideas.retrieveIdeas();
+  Ideas.ideasToPage();
   clearInput();
 });
 
-//need to make id equal to the random number date.now() creates
-//remove idea from array, localStorage, and page
-$ideaContainer.on('click', '.remove-idea', function() {
-  $(this).closest('article').remove();
-  findIdeaById();
-});
+var Ideas = {
+  ideasArray: [],
+  generateNewIdea: function(title, body) {
+    var $titleInput = $('.title-input').val();
+    var $bodyInput = $('.body-input').val();
+    this.ideasArray.push(new Idea($titleInput, $bodyInput));
+    this.storeTheArray();
+  },
+  remove: function(id) {
+    $ideaContainer.on('click', '.remove-idea', function() {
+      $(this).closest('article').remove();
+    });
+  },
+  storeTheArray: function() {
+    localStorage.setItem('ideasArray', JSON.stringify(this.ideasArray));
+    this.ideasToPage();
+  },
+  retrieveIdeas: function() {
+    var storedIdeas = JSON.parse(localStorage.getItem('ideasArray'));
+    if (storedIdeas) { ideasArray = storedIdeas; }
+    this.ideasToPage();
+  },
+  ideasToPage: function() {
+    $('.idea-container').html('');
+    // var storedIdea = this.ideasArray;
+    this.ideasArray.map(function(generateNewIdea)
+      return generateNewIdea.htmlLayout();
+    // $('.idea-container').html(this.ideasArray.forEach(function(ideasToPage));
+  },
+
+  findIdeaById: function(id) {
+    return this.ideasArray.find(function (idea) {
+      return idea.id === id;
+    });
+  }
+};
+
+Idea.prototype.htmlLayout = function() {
+  return $(`
+          <article class="idea" id=${Ideas.ideasArray.id}>
+            <header class= "idea-header">
+              <h3 class="idea-title">${Ideas.ideasArray.title}</h3>
+                <section class="idea-header-buttons">
+                  <button class="remove-idea" type="button"></button>
+                </section>
+            </header>
+            <body class="idea-body">
+              <p class="idea-body">${Ideas.ideasArray.body}</p>
+            </body>
+            <footer class="idea-footer">
+              <button class="thumbs-up" type="button"></button>
+              <button class="thumbs-down" type="button"></button>
+              <p>ranking: <span class="ranking">${Ideas.ideasArray.quality}</span></p>
+            </footer>
+          </article>`).prependTo($ideaContainer);
+  };
+
+function clearInput() {
+  $('.title-input').val('');
+  $('.body-input').val('');
+  $('.title-input').focus();
+}
 
 function findIdeaById() {
   var id = Date.now();
@@ -109,10 +118,6 @@ function findIdeaById() {
     }
   }
 }
-
-$ideaContainer.on('click', '.thumbs-up', thumbsUpRanking);
-
-$ideaContainer.on('click', '.thumbs-down', thumbsDownRanking);
 
 $(document).ready(function() {
   $('.search-input').keyup(function() {
